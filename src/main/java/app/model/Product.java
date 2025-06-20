80834bb983b3600ed38af977102b156e19daff17
Alex Balak <alex.balak@outlook.com>
2025-06-21 00:34:18 +0200

    Created Product Model & ProductRepository

---
+++ src/main/java/app/model/Product.java
@@ -0,0 +1,31 @@
+package app.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+}
