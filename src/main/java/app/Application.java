65fdb500ba6756ef072f364d673446a1b53f19d3
Alex Balak <alex.balak@outlook.com>
2025-06-21 00:02:49 +0200

    Initial application Bootstraped & working

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootJwtAuthRefreshApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootJwtAuthRefreshApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
